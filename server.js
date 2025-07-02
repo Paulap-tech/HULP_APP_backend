2f0a5a2f09ce9ad349426e699e980884b4467e48
Diego Moreno <diego.morenob@upm.es>
2025-07-02 10:05:03 +0200

    Migrado a posgre

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const mysql = require('mysql2');
+const { Pool } = require('pg');
 const cors = require('cors');
 const moment = require('moment');
 
@@ -8,27 +8,27 @@ app.use(cors());
 app.use(express.json()); // Maneja JSON
 app.use(express.urlencoded({ extended: true })); // Maneja datos codificados en URL
 
-const db = mysql.createConnection({
+const db = new Pool({
+    user: 'hulp', // usuario de tu base de datos
     host: 'localhost',
-    user: 'root',
-    password: '@@@@@@@',
-    database: 'HULPAPP',
-    port: 3306
+    database: 'HULPAPP', // nombre de tu base de datos
+    password: 'pw_4_hulp', // tu contraseña
+    port: 5432 // puerto por defecto de PostgreSQL
 });
 
-db.connect((err) => {
-    if (err) {
-        console.error('Error connecting to database: ' + err.stack);
-        return;
-    }
-    console.log('Connected to database as id ' + db.threadId);
-});
+// db.connect((err) => {
+//     if (err) {
+//         console.error('Error connecting to database: ' + err.stack);
+//         return;
+//     }
+//     console.log('Connected to database as id ' + db.threadId);
+// });
 
 // Ruta para registrar un nuevo usuario
 app.post('/registro', (req, res) => {
     const { email, password, age, ano_diagnostico, ano_sintomas } = req.body;
 
-    db.query('SELECT * FROM usuarios WHERE email = ?', [email], (err, results) => {
+    db.query('SELECT * FROM usuarios WHERE email = $1', [email], (err, results) => {
         if (err) {
             console.error('Error al consultar el usuario:', err);
             return res.status(500).json({ message: 'Error en el servidor.' });
@@ -37,7 +37,7 @@ app.post('/registro', (req, res) => {
         if (results.length > 0) {
             return res.status(409).json({ message: 'El usuario o correo electrónico ya están registrados.' });
         } else {
-            db.query('INSERT INTO usuarios (email, password, age, ano_diagnostico, ano_sintomas) VALUES (?, ?, ?, ?, ?)', [email, password, age, ano_diagnostico,ano_sintomas], (err, result) => {
+            db.query('INSERT INTO usuarios (email, password, age, ano_diagnostico, ano_sintomas) VALUES ($1, $2, $3, $4, $5)', [email, password, age, ano_diagnostico,ano_sintomas], (err, result) => {
                 if (err) {
                     console.error('Error al insertar el usuario:', err);
                     return res.status(500).json({ message: 'Error en el servidor.' });
@@ -63,7 +63,7 @@ app.post('/api/ciclo-duracion', (req, res) => {
         return res.status(400).json({ message: 'La duración debe ser un número positivo.' });
     }
 
-    const query = 'UPDATE usuarios SET cicle_days = ? WHERE email = ?';
+    const query = 'UPDATE usuarios SET cicle_days = $1 WHERE email = $2';
     db.query(query, [duration, email], (err, results) => {
         if (err) {
             console.error('Error al actualizar la duración del ciclo:', err);
@@ -83,7 +83,7 @@ app.post ('/information', (req, res) => {
     if (!email || !talla || !tratamiento || !peso) {
         return res.status(400).json({ message: 'Todos los campos son requeridos.' });
     }
-    const query = 'UPDATE usuarios SET talla = ?, tratamiento = ?, peso = ? WHERE email = ?';
+    const query = 'UPDATE usuarios SET talla = $1, tratamiento = $2, peso = $3 WHERE email = $4';
     db.query(query, [talla, tratamiento, peso, email], (err, results) => {
         if (err) {
             console.error('Error al guardar la información:', err);
@@ -98,14 +98,14 @@ app.post ('/information', (req, res) => {
 app.post('/login', (req, res) => {
     const { email, password } = req.body;
 
-    db.query('SELECT * FROM usuarios WHERE email = ? AND password = ?', [email, password], (err, results) => {
+    db.query('SELECT * FROM usuarios WHERE email = $1 AND password = $2', [email, password], (err, results) => {
         if (err) {
             return res.status(500).json({ message: 'Error interno del servidor.' });
         }
-
-        if (results.length > 0) {
+        console.log('Resultados de la consulta:', results); // Log para depuración
+        if (results.rowCount > 0) {
             // Asegúrate de que la respuesta incluya todos los campos necesarios
-            return res.status(200).json(results[0]);
+            return res.status(200).json(results.rows[0]);
         } else {
             return res.status(401).json({ message: 'Credenciales incorrectas.' });
         }
@@ -117,14 +117,14 @@ app.post('/login', (req, res) => {
 app.get('/usuario/:email', (req, res) => {
     const { email } = req.params;
 
-    db.query('SELECT * FROM usuarios WHERE email = ?', [email], (err, results) => {
+    db.query('SELECT * FROM usuarios WHERE email = $1', [email], (err, results) => {
         if (err) {
             console.error('Error al obtener datos del usuario:', err);
             return res.status(500).json({ error: 'Error interno del servidor' });
         }
 
-        if (results.length > 0) {
-            const userData = results[0];
+        if (results.rowCount > 0) {
+            const userData = results.rows[0];
             res.status(200).json(userData);
         } else {
             res.status(404).json({ message: 'Usuario no encontrado' });
@@ -136,15 +136,15 @@ app.get('/usuario/:email', (req, res) => {
 app.get('/ciclo/:email', (req, res) => {
     const email = req.params.email;
   
-    const query = 'SELECT cicle_days FROM usuarios WHERE email = ?';
+    const query = 'SELECT cicle_days FROM usuarios WHERE email = $1';
     db.query(query, [email], (err, results) => {
       if (err) {
         console.error('Error al obtener los detalles del usuario:', err);
         res.status(500).json({ error: 'Database error' });
-      } else if (results.length === 0) {
+      } else if (results.rowCount === 0) {
         res.status(404).json({ error: 'Usuario no encontrado' });
       } else {
-        res.status(200).json(results[0]);
+        res.status(200).json(results.rows[0]);
       }
     });
   });
@@ -156,14 +156,14 @@ app.get('/registros/fechas/:email', (req, res) => {
         return res.status(400).json({ message: 'El correo electrónico es requerido.' });
     }
 
-    const query = 'SELECT fecha_inicio, fecha_fin, fase FROM menstruacion WHERE email = ?';
+    const query = 'SELECT fecha_inicio, fecha_fin, fase FROM menstruacion WHERE email = $1';
     db.query(query, [email], (err, results) => {
         if (err) {
             console.error('Error al obtener las fechas:', err);
             return res.status(500).json({ message: 'Error interno del servidor.' });
         }
-
-        res.status(200).json(results);
+        console.log('Resultados de la consulta:', results);
+        res.status(200).json(results.rows);
     });
 });
 
@@ -176,7 +176,7 @@ app.post('/menstruacion', (req, res) => {
         return res.status(400).json({ error: 'Todos los campos son requeridos.' });
     }
 
-    const query = 'INSERT INTO menstruacion (email, fecha_inicio, fecha_fin, fase) VALUES (?, ?, ?, ?)';
+    const query = 'INSERT INTO menstruacion (email, fecha_inicio, fecha_fin, fase) VALUES ($1, $2, $3, $4)';
     const values = [email, fecha_inicio, fecha_fin, fase ];
 
     db.query(query, values, (err, result) => {
@@ -192,9 +192,10 @@ app.post('/menstruacion', (req, res) => {
 app.post('/fases', (req, res) => {
     const { email, fecha_inicio, fecha_fin, fase } = req.body;
   
+    console.log('Datos recibidos para fase lútea:', { email, fecha_inicio, fecha_fin, fase });
     const query = `
       INSERT INTO menstruacion (email, fecha_inicio, fecha_fin, fase)
-      VALUES (?, ?, ?, ?)
+      VALUES ($1, $2, $3, $4)
     `;
     db.query(query, [email, fecha_inicio, fecha_fin, fase], (err, results) => {
       if (err) {
@@ -216,18 +217,18 @@ app.get('/fase-menstrual/:email/:date', (req, res) => {
         return res.status(400).json({ message: 'El correo electrónico y la fecha son requeridos.' });
     }
 
-    const query = 'SELECT fase FROM menstruacion WHERE email = ? AND fecha_inicio <= ? AND fecha_fin >= ?';
+    const query = 'SELECT fase FROM menstruacion WHERE email = $1 AND fecha_inicio <= $2 AND fecha_fin >= $3';
     db.query(query, [email, date, date], (err, results) => {
         if (err) {
             console.error('Error al obtener la fase menstrual:', err);
             return res.status(500).json({ message: 'Error interno del servidor.' });
         }
 
-        if (results.length === 0) {
+        if (results.rowCount === 0) {
             return res.status(404).json({ message: 'No se encontró fase menstrual para esta fecha.' });
         }
 
-        res.status(200).json({ fase: results[0].fase });
+        res.status(200).json({ fase: results.rows[0].fase });
     });
 });
 
@@ -240,18 +241,18 @@ app.post('/registros', (req, res) => {
     }
 
     // Primero obtenemos la fase menstrual
-    const getFaseQuery = 'SELECT fase FROM menstruacion WHERE email = ? AND fecha_inicio <= ? AND fecha_fin >= ?';
+    const getFaseQuery = 'SELECT fase FROM menstruacion WHERE email = $1 AND fecha_inicio <= $2 AND fecha_fin >= $3';
     db.query(getFaseQuery, [email, fecha, fecha], (err, results) => {
         if (err) {
             console.error('Error al obtener la fase menstrual:', err);
             return res.status(500).json({ message: 'Error interno del servidor.' });
         }
 
-        const faseMenstrual = results.length > 0 ? results[0].fase : null;
+        const faseMenstrual = results.rowCount > 0 ? results.rows[0].fase : null;
 
         // Ahora guardamos el registro con la fase menstrual
-        const insertQuery = 'INSERT INTO registros (date, faseMenstrual, cataplejiaExtremidades, cataplejiaFacial, cataplejiaSuelo, suenoFragmentado, paralisisSuenio, somnolencia, email) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)';
-        db.query(insertQuery, [fecha, faseMenstrual, cataplejiaExtremidades, cataplejiaFacial, cataplejiaSuelo, suenoFragmentado, paralisisSuenio, somnolencia, email, ''], (err, results) => {
+        const insertQuery = 'INSERT INTO registros (date, faseMenstrual, cataplejiaExtremidades, cataplejiaFacial, cataplejiaSuelo, suenoFragmentado, paralisisSuenio, somnolencia, email) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)';
+        db.query(insertQuery, [fecha, faseMenstrual, cataplejiaExtremidades, cataplejiaFacial, cataplejiaSuelo, suenoFragmentado, paralisisSuenio, somnolencia, email], (err, results) => {
             if (err) {
                 console.error('Error al guardar el registro:', err);
                 return res.status(500).json({ message: 'Error interno del servidor.' });
